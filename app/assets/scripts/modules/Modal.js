36df953dad1b9c3cad2679bf887012cc9aafd7f5
joro <georgi.bultag@gmail.com>
2019-08-06 14:49:23 +0300

    Mah imageh all dane

---
+++ app/assets/scripts/modules/Modal.js
@@ -0,0 +1,36 @@
+import $ from 'jquery';
+
+class Modal {
+	constructor() {
+		this.openModalButton = $(".open-modal");
+		this.modal = $(".modal");
+		this.closeModalButton = $(".modal__close");
+		this.events();
+	}
+
+	events() {
+		// clocking the open button
+		this.openModalButton.click(this.openModal.bind(this));
+		// clicking the x
+		this.closeModalButton.click(this.closeModal.bind(this));
+		// pushes any key    
+		$(document).keyup(this.keyPressHandler.bind(this));
+	}
+
+	keyPressHandler(e) {
+		if (e.keyCode == 27) {
+			this.closeModal();
+		}
+	}
+
+	openModal() {
+		this.modal.addClass("modal--is-visible");
+		return false;
+	}
+
+	closeModal() {
+		this.modal.removeClass("modal--is-visible");
+	}	
+}
+
+export default Modal;
